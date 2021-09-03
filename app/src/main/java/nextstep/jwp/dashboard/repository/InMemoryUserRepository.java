10fd9787321c0709f30f87696a4c2cc2855c629b
kimjunseo <air.junseo@gmail.com>
2021-09-03 16:18:10 +0900

    refactor: 회원가입시 id값 추가

--- app/src/main/java/nextstep/jwp/dashboard/repository/InMemoryUserRepository.java
+++ app/src/main/java/nextstep/jwp/dashboard/repository/InMemoryUserRepository.java
@@ -8,14 +8,16 @@ import java.util.concurrent.ConcurrentHashMap;
 import nextstep.jwp.dashboard.domain.User;
 
 public class InMemoryUserRepository {
+    private static Long seq = 0L;
     private static final Map<String, User> database = new ConcurrentHashMap<>();
 
     static {
-        final User user = new User(1L, "gugu", "password", "hkkang@woowahan.com");
+        final User user = new User(++seq, "gugu", "password", "hkkang@woowahan.com");
         database.put(user.getAccount(), user);
     }
 
     public void save(User user) {
+        user.setId(++seq);
         database.put(user.getAccount(), user);
     }
 
